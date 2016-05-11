a9f2aab1e580cf10f77855d8e3228f2d669b835a
Rafael Castrillo <rcastrillob@gmail.com>
2016-05-11 17:03:01 +0200

    programs

--- panel_data.R
+++ panel_data.R
@@ -1,6 +1,8 @@
+#version 3.2.1
+
 rm(list=ls())
 
-packages <- c("ggplot2", "dplyr", "plm","reshape2", "AER","stargazer")
+packages <- c("ggplot2", "dplyr", "plm","reshape2", "AER","stargazer","xtable")
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
         install.packages(setdiff(packages, rownames(installed.packages())))  
 }
@@ -11,6 +13,7 @@ library(plm)
 library(reshape2)
 library(AER)
 library(stargazer)
+library(xtable)
 
 setwd("~/UvA - MsC Economics/Thesis/HTMLs")
 source("read_data.R")
@@ -25,9 +28,10 @@ data$id <- paste(data$trip,data$When,data$Downloaded,sep=".")
 group <- split(data,data$id)
 fullBuses <- sapply(group,function(x){sum(x$isFull)})
 daysToTrip<-sapply(group,function(x){x$DaysToTrip[1]})
-price <- sapply(group,function(x){z <- x[!x$isFull,];median(as.numeric(z$Price)
+price <- sapply(group,function(x){z <- x[!x$isFull,];mean(as.numeric(z$Price)
                                                           ,na.rm=TRUE)})
-competitors <- sapply(group, function(x){z <- x[!x$isFull,];NROW(unique(z$Companies))})
+competitors <- sapply(group, function(x){z <- x[(!x$isFull)&(!x$multiple),];
+                NROW(unique(z$Companies))})
 tripLength <- sapply(group,function(x){mean(x$TripLength)})
 weekdays <-sapply(group,function(x){x$weekday[1]})
 tripDay <- names(fullBuses)
@@ -38,8 +42,8 @@ panel$when <- as.Date(as.character(panel$when))
 time <- daysToTrip + 46
 panel <-cbind(panel,price, competitors, tripLength, weekdays, time,
               fullBuses)
-panel <-pdata.frame(panel, index = c("id", "time"), drop.index = TRUE, 
-                    row.names = TRUE)
+#panel <-pdata.frame(panel, index = c("id", "time"), drop.index = TRUE, 
+#                    row.names = TRUE)
 
 trips <- as.character(unique(panel$trip))
 
@@ -50,18 +54,32 @@ colnames(results) <- trips
 for(i in 1:8){
         print(trips[i])
         dat <- panel[panel$trip == trips[i],]
-        model <- ivreg(price ~ competitors + time 
+        model <- ivreg(log(price) ~ competitors + time 
                        + I(weekdays)  +when | time + I(weekdays) + when +fullBuses,
                        data = dat) 
         print(summary(model, vcov = sandwich, diagnostics = TRUE))
         results[1,i ] <- model$coefficients[2]
         results[2,i ] <-  summary(model,diagnostics=TRUE, 
-                                     vcov = sandwich)$coefficients[2,4]
+                                  vcov = sandwich)$coefficients[2,4]
         results[3:4, i] <- summary(model,diagnostics=TRUE, 
-                                       vcov = sandwich)$diagnostics[1:2,4]
+                                   vcov = sandwich)$diagnostics[1:2,4]
 }
 results <- round(results,3)
 results
+xtable(results)
 
 
+extremes <- lapply(group, function(x){z <- x[!x$isFull,]; z <- z[order(z$Price),];cbind(as.character(z$Companies[1]),as.character(z$Companies[NROW(z)]))})
+extremes <- data.frame(cbind(panel$trip, do.call(rbind, extremes)))
+colnames(extremes)<- c("trip", "most_expensive", "cheapest")
+
+cheap_tab <- table(extremes$cheapest,extremes$trip)
+cheap_prop_tab <- round(prop.table(cheap_tab,2)*100,2)
+cheap_prop_tab <- cheap_prop_tab[rowSums(cheap_prop_tab)>5,]
+cheap_prop_tab <- rbind(cheap_prop_tab,Other = 100 - colSums(cheap_prop_tab))
+
+expensive_tab <- table(extremes$most_expensive,extremes$trip)
+expensive_prop_tab <- round(prop.table(expensive_tab,2)*100,2)
+expensive_prop_tab <- expensive_prop_tab[rowSums(expensive_prop_tab)>5,]
+expensive_prop_tab <- rbind(expensive_prop_tab,Other = 100 - colSums(expensive_prop_tab))
 
--- read_data.R
+++ read_data.R
@@ -1,5 +1,5 @@
 data <- read.csv("~/UvA - MSc Economics/Thesis/HTMLs/Data.csv",stringsAsFactors=FALSE)
-data <-data[-grep("DeutscheBahn|bokolina|Thalys|oyages-sncf.com|SNCBEurope",data$Companies),]
+data <-data[-grep("DeutscheBahn|bokolina|Thalys|oyages-sncf.com|AirBerlin|SNCBEurope",data$Companies),]
 data$trip<-paste0(paste0(data$From ,"-",""),data$To,"")
 data$weekday <-weekdays(as.Date(data$When))
 data$Companies <- as.factor(data$Companies)
@@ -12,6 +12,7 @@ data$Downloaded <- as.Date(data$Downloaded,"%d-%m-%y")
 data$Anticipation<-data$When - data$Downloaded
 data$DaysToTrip <- -as.numeric(data$Anticipation)
 data$isFull <- grepl("*Completo",data$Price)== TRUE
+data$multiple <- grepl("[&]",data$Companies)
 duplicated<-duplicated(data.frame(data$Departure,data$Companies,data$trip,data$Downloaded))
 data<-data[!duplicated,]
 rm(duplicated)
@@ -22,3 +23,7 @@ data$DepartureTime<-(strptime(substr(data$Departure,1,15),"%d %b | %R",tz="Europ
 data$TripLength <- data$ArrivalTime - data$DepartureTime 
 data$TripLength[data$To == "london"] <-data$TripLength[data$To == "london"] + 1
 data$id <- paste(data$trip,data$When,data$Downloaded,sep=".")
+
+#data$Companies[grepl("*urolines",data$Companies)== TRUE] <- "Eurolines"
+
+data <- data[complete.cases(data$TripLength),]
